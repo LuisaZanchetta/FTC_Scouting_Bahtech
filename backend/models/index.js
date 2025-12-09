6ae66ee713c074bdf36a49f4a1b77273c0d993b0
LuisaZanchetta <89790770+LuisaZanchetta@users.noreply.github.com>
2025-12-09 12:07:55 -0300

    Set up model initialization and relationships
    Initialize models and set up relationships between Team, Match, and ScoutEntry.

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,1 +1,17 @@
+import User from "./user.js";
+import Team from "./team.js";
+import Match from "./match.js";
+import ScoutEntry from "./scoutEntry.js";
 
+export default (sequelize) => {
+User.initModel(sequelize);
+Team.initModel(sequelize);
+Match.initModel(sequelize);
+ScoutEntry.initModel(sequelize);
+
+Team.hasMany(ScoutEntry);
+ScoutEntry.belongsTo(Team);
+
+Match.hasMany(ScoutEntry);
+ScoutEntry.belongsTo(Match);
+};

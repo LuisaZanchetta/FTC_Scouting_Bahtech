6a6117c66884ed24867dfcf5029281cd977de8eb
LuisaZanchetta <89790770+LuisaZanchetta@users.noreply.github.com>
2025-12-09 12:06:48 -0300

    Add dotenv configuration for environment variables

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,10 @@
+import dotenv from "dotenv";
+dotenv.config();
 
+export default {
+jwtSecret: process.env.JWT_SECRET || "supersecretkey",
+database: {
+dialect: "sqlite",
+storage: "./db.sqlite"
+}
+}

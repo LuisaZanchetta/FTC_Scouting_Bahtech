ffcc3800df592de38decfda505860c77ba6800e4
LuisaZanchetta <89790770+LuisaZanchetta@users.noreply.github.com>
2025-12-09 12:08:50 -0300

    Add Match model with initialization method

--- backend/models/match.js
+++ backend/models/match.js
@@ -1,1 +1,13 @@
+import { DataTypes, Model } from "sequelize";
 
+export default class Match extends Model {
+static initModel(sequelize) {
+Match.init(
+{
+matchNumber: DataTypes.INTEGER,
+event: DataTypes.STRING
+},
+{ sequelize, modelName: "match" }
+);
+}
+}

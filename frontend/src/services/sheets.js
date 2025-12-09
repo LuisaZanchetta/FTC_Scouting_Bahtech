ebd2c56b532ee8a61e65bad1507f8b04a84596b1
LuisaZanchetta <89790770+LuisaZanchetta@users.noreply.github.com>
2025-12-09 13:43:09 -0300

    Update API_URL to point to the correct endpoint

--- frontend/src/services/sheets.js
+++ frontend/src/services/sheets.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "SUA_URL_DO_APPS_SCRIPT_AQUI";
+const API_URL = "https://script.google.com/macros/s/AKfycbyU_6mDlW12lmnLY0Khfjj_2d7tjFGYcux-LtUaLMGh4vAe6znGc0hotERTM7ZsZp3a/exec";
 
 export function sendScoutEntry(data) {
   return axios.post(API_URL, data);

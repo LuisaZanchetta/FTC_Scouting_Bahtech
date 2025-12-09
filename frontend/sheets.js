acab025481811ca1f98fb515dc783dd8ba7a5b7c
LuisaZanchetta <89790770+LuisaZanchetta@users.noreply.github.com>
2025-12-09 13:48:06 -0300

    Add sheets.js for API interaction functions

---
+++ frontend/sheets.js
@@ -0,0 +1,9 @@
+const API_URL = "https://script.google.com/macros/s/AKfycbyU_6mDlW12lmnLY0Khfjj_2d7tjFGYcux-LtUaLMGh4vAe6znGc0hotERTM7ZsZp3a/exec";
+
+function sendScoutEntry(data) {
+  return axios.post(API_URL, data);
+}
+
+function fetchAllEntries() {
+  return axios.get(API_URL);
+}

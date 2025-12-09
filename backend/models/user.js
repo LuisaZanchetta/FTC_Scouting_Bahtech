43f002de588ae042eccf915ff39b48b27046bc61
LuisaZanchetta <89790770+LuisaZanchetta@users.noreply.github.com>
2025-12-09 12:08:14 -0300

    Implement User model with Sequelize

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,1 +1,13 @@
+import { DataTypes, Model } from "sequelize";
 
+export default class User extends Model {
+static initModel(sequelize) {
+User.init(
+{
+username: DataTypes.STRING,
+password: DataTypes.STRING
+},
+{ sequelize, modelName: "user" }
+);
+}
+}

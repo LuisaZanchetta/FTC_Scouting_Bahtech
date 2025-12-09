5c82fa97a654f90e1ac0c4df7a632067d1e160c0
LuisaZanchetta <89790770+LuisaZanchetta@users.noreply.github.com>
2025-12-09 12:08:31 -0300

    Add Team model with number and name attributes

--- backend/models/team.js
+++ backend/models/team.js
@@ -1,1 +1,13 @@
+import { DataTypes, Model } from "sequelize";
 
+export default class Team extends Model {
+static initModel(sequelize) {
+Team.init(
+{
+number: DataTypes.INTEGER,
+name: DataTypes.STRING
+},
+{ sequelize, modelName: "team" }
+);
+}
+}

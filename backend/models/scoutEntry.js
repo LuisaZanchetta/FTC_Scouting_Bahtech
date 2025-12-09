b6d0ac118535dbfe13cecaa3c94248e49fd6f5ba
LuisaZanchetta <89790770+LuisaZanchetta@users.noreply.github.com>
2025-12-09 12:09:25 -0300

    Add ScoutEntry model with score attributes

--- backend/models/scoutEntry.js
+++ backend/models/scoutEntry.js
@@ -1,1 +1,15 @@
+import { DataTypes, Model } from "sequelize";
 
+export default class ScoutEntry extends Model {
+static initModel(sequelize) {
+ScoutEntry.init(
+{
+autonomousScore: DataTypes.INTEGER,
+teleopScore: DataTypes.INTEGER,
+endgameScore: DataTypes.INTEGER,
+notes: DataTypes.STRING
+},
+{ sequelize, modelName: "scoutEntry" }
+);
+}
+}
